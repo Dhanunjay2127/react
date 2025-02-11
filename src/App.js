bfde70c8a139d833c346ad1f257be946ce011dd5
Dhanunjay2127 <dhanunjaya@mobil80.com>
2025-02-11 10:40:29 +0530

    Routes

--- src/App.js
+++ src/App.js
@@ -1,30 +1,19 @@
+import { Routes, Route } from "react-router-dom";
 import "./App.css";
-import TodoList from "./components/TodoList";
-import TodoUser from "./components/TodoUser";
+import Todo from "./components/Todo";
 import Board from "./components/Board";
 import UserProfile from "./components/UserProfile";
 import ContextAcess from "./components/ContextAcess";
-import { CounterProvider } from "./components/context";
 
 function App() {
   return (
     <>
-      <div style={{ display: "flex", gap: "20px", maxWidth: "1200px", flexWrap: "wrap" }}>
-        <CounterProvider>
-          <div className="todo">
-            <TodoUser />
-            <h3>Todo list</h3>
-            <TodoList item="Eat" />
-            <TodoList item="Code" />
-            <TodoList item="Sleep" />
-            <TodoList item="Repeat" />
-            <button className="todo-button">Add item</button>
-          </div>
-          <Board />
-          <UserProfile />
-          <ContextAcess />
-        </CounterProvider>
-      </div>
+      <Routes>
+        <Route path="/" element={<Todo />} />
+        <Route path="/board" element={<Board />} />
+        <Route path="/user" element={<UserProfile />} />
+        <Route path="/context" element={<ContextAcess />} />
+      </Routes>
     </>
   );
 }
